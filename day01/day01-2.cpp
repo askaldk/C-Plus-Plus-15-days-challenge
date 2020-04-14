45bb158e0c1687d4b2af635f71882f58298d02f6
李昂謙 <aska0522@gmail.com>
2020-04-14 19:55:19 +0800

    more cout

---
+++ day01/day01-2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    cout<<"Primitive : double"<<endl;
+    cout<<"Primitive : char"<<endl;
+    cout<<"Primitive : boolean"<<endl;
+    cout<<"Primitive : int"<<endl;
+    cout<<"Referenced : String"<<endl;
+    cout<<"Primitive : double"<<endl;
+    cout<<"Primitive : char"<<endl;
+    cout<<"Referenced : String"<<endl;
+    cout<<"Primitive : boolean"<<endl;
+}
