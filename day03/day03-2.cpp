23106ca6eb2eff478afdc2d561a6d16dee6a1372
李昂謙 <aska0522@gmail.com>
2020-04-28 21:33:27 +0800

    vector manipulate

---
+++ day03/day03-2.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+class Case
+{
+public:
+    int a, b, n;
+    Case(int _a, int _b, int _n)
+    {
+        a = _a;
+        b = _b;
+        n = _n;
+    }
+};
+int main()
+{
+    int t;
+    cin >> t;
+    vector<Case> cases;
+    for (int i = 0; i < t; i++)
+    {
+        int a, b, n;
+        cin >> a >> b >> n;
+        Case _case(a, b, n);
+        cases.push_back(_case);
+    }
+    for (int i = 0; i < t; i++)
+    {
+        int last = cases[i].a;
+        for (int j = 0; j < cases[i].n; j++)
+        {
+            last = last + (cases[i].b << j);
+            cout << last << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
